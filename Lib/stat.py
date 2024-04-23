0c8c3120c0bf2b37d88eae112aa81008e75802d2
Frederick Price <rprice@pricemail.ca>
2024-04-23 13:07:35 -0400

    CVE-2007-4559 Update code to work with Python2
    Note that Python2 uses str as a bytearray, and Python3 uses a real
    bytearra. This causes problems when you slice a bytearray and expect to
    get an int back, but you get a char instead.

--- Lib/stat.py
+++ Lib/stat.py
@@ -94,3 +94,43 @@ SF_IMMUTABLE = 0x00020000
 SF_APPEND    = 0x00040000
 SF_NOUNLINK  = 0x00100000
 SF_SNAPSHOT  = 0x00200000
+
+_filemode_table = (
+    ((S_IFLNK,         "l"),
+     (S_IFREG,         "-"),
+     (S_IFBLK,         "b"),
+     (S_IFDIR,         "d"),
+     (S_IFCHR,         "c"),
+     (S_IFIFO,         "p")),
+
+    ((S_IRUSR,         "r"),),
+    ((S_IWUSR,         "w"),),
+    ((S_IXUSR|S_ISUID, "s"),
+     (S_ISUID,         "S"),
+     (S_IXUSR,         "x")),
+
+    ((S_IRGRP,         "r"),),
+    ((S_IWGRP,         "w"),),
+    ((S_IXGRP|S_ISGID, "s"),
+     (S_ISGID,         "S"),
+     (S_IXGRP,         "x")),
+
+    ((S_IROTH,         "r"),),
+    ((S_IWOTH,         "w"),),
+    ((S_IXOTH|S_ISVTX, "t"),
+     (S_ISVTX,         "T"),
+     (S_IXOTH,         "x"))
+)
+
+def filemode(mode):
+    """Convert a file's mode to a string of the form '-rwxrwxrwx'."""
+    perm = []
+    for table in _filemode_table:
+        for bit, char in table:
+            if mode & bit == bit:
+                perm.append(char)
+                break
+        else:
+            perm.append("-")
+    return "".join(perm)
+
